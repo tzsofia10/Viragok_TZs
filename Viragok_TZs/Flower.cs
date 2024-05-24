e07ddf5cfa9ce87e961b2c036b48056f0048d1f7
TothZsofia <tzsofia07@gmail.com>
2024-05-24 08:00:31 +0200

    Add project files.

---
+++ Viragok_TZs/Flower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viragok_TZs
+{
+    internal class Flower
+    {
+        public string Fajta { get; set; }
+        public int Ar { get; set; }
+        public string Szine { get; set; }
+       
+
+        public Flower(string sorok) 
+        {
+            var sor = sorok.Split(";");
+            Fajta = sor[0];
+            Ar = Convert.ToInt32(sor[1]);
+            Szine= sor[2];
+            
+        }
+    }
+}
---
+++ Viragok_TZs/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Viragok_TZs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Flower> viraaag = new List<Flower>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\flowers.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            while (!sr.EndOfStream)
+            {
+                viraaag.Add(new Flower(sr.ReadLine()));
+            }
+            sr.Close();
+
+            foreach (var item in viraaag)
+            {
+                viragnev.Items.Add(item.Fajta);
+                
+                viragar.Items.Add(item.Ar);
+                viragszin.Items.Add(item.Szine);
+                
+            }
+
+            //abc sorrendbe rendezés próbálkozás de nem működik
+            viragnev.Items.SortDescriptions.Add(new SortDescription("Content", ListSortDirection.Ascending));
+        }
+
+        private void torles_Click(object sender, RoutedEventArgs e)
+        {
+            if (viragnev.SelectedItem != null)
+            {
+                viragnev.Items.Remove(viragnev.SelectedItem);
+            }
+            if (viragar.SelectedItem != null)
+            {
+                viragar.Items.Remove(viragar.SelectedItem);
+            }
+            if (viragszin.SelectedItem != null)
+            {
+                viragszin.Items.Remove(viragszin.SelectedItem);
+            }
+        }
+
+        private void masolas_Click(object sender, RoutedEventArgs e)
+        {
+            if (viragnev.SelectedItem!=null)
+            {
+                viragmasolas.Items.Add(viragnev.SelectedItem);
+            }
+        }
+    }
+}
